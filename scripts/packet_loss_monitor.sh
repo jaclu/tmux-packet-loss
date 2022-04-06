10806a502a00d402b1b98e6d2806ca7b0db7578b
jaclu <jacob.lundqvist@gmail.com>
2022-04-06 03:19:38 +0200

    Simplified extraction of loss from ping output, and added explanation of this process

--- scripts/packet_loss_monitor.sh
+++ scripts/packet_loss_monitor.sh
@@ -7,7 +7,7 @@
 #
 #   Part of https://github.com/jaclu/tmux-packet-loss
 #
-#   Version: 0.1.3 2022-04-04
+#   Version: 1.1.4 2022-04-06
 #
 
 
@@ -100,8 +100,13 @@ while : ; do
         #  We cant rely on the absolute position of the %loss, since sometimes it is prepended with stuff like:
         #  "+1 duplicates,"
         #  To handle this we search for "packet loss" and use the word just before it.
+        #  1 Only bother with the line containing the word loss
+        #  2 replace "packet loss" with ~, since cut needs a single char delimiter
+        #  3 remove any % chars, we want loss as a float
+        #  4 only keep line up to not including ~ (packet loss)
+        #  5 display last remaining word - packet loss as a float with no % sign!
         #
-        percent_loss="$(echo "$output" | sed 's/packet loss/\|/' | cut -d\| -f 1 | awk 'NF>1{print $NF}' | sed s/%// )"
+        percent_loss="$(echo "$output" | sed 's/packet loss/~/ ; s/%//' | cut -d~ -f 1 | awk 'NF>1{print $NF}' )"
         if [ -z "$percent_loss" ]; then
             error_msg "Failed to parse ping output, unlikely to self correct!"
             percent_loss="$error_unable_to_detect_loss"
@@ -120,5 +125,5 @@ while : ; do
         sleep "$ping_count"
     fi
     sqlite3 "$db" "INSERT INTO packet_loss (loss) values ($percent_loss)"
-    log_it "stored in DB [$percent_loss]"
+    log_it "stored in DB: $percent_loss"
 done

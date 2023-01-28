1b6e4b9608e6751a75d1e38a26c304e1759dd7b1
jaclu <jacob.lundqvist@gmail.com>
2023-01-28 18:10:54 +0100

    Added historical average feature

--- scripts/check_packet_loss.sh
+++ scripts/check_packet_loss.sh
@@ -15,20 +15,21 @@ CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 # shellcheck disable=SC1091
 . "$CURRENT_DIR/utils.sh"
 
-db="$(dirname -- "$CURRENT_DIR")/data/$sqlite_db"
-
-log_file_base="/tmp/packet-loss"
-log_file_db_missing="${log_file_base}-missing.log"
-log_file_db_old="${log_file_base}-old.log"
-
 restart_monitor() {
     log_it "restarting monitor"
     parrent_dir="$(dirname "$CURRENT_DIR")"
     "$parrent_dir/packet-loss.tmux" stop
     "$parrent_dir/packet-loss.tmux"
-    sleep 1  # give the first check time to complete
+    sleep 1 # give the first check time to complete
 }
 
+get_settings
+
+db="$(dirname -- "$CURRENT_DIR")/data/$sqlite_db"
+
+log_file_base="/tmp/packet-loss"
+log_file_db_missing="${log_file_base}-missing.log"
+log_file_db_old="${log_file_base}-old.log"
 
 #
 #  Some sanity check, ensuring the monitor is running
@@ -39,7 +40,7 @@ if [ ! -e "$db" ]; then
     #
     #  If DB is missing, try to start the monitor
     #
-    date >> "$log_file_db_missing" # for now log actions
+    date >>"$log_file_db_missing" # for now log actions
     restart_monitor
     if [ ! -e "$db" ]; then
         log_it "repeated fails DB missing"
@@ -54,13 +55,11 @@ if [ -n "$(find "$db" -mmin +1)" ]; then
     #  If DB is over a minute old,
     #  assume the monitor is not running, so start it
     #
-    date >> "$log_file_db_old" # for now log actions
+    date >>"$log_file_db_old" # for now log actions
     restart_monitor
 fi
 
-
-
-if bool_param "$(get_tmux_option "@packet-loss_weighted_average" "$default_weighted_average")"; then
+if bool_param "$is_weighted_avg"; then
     # weighted_average=1
     #
     #  To give loss a declining history weighting, it is displayed as the largest of:
@@ -86,17 +85,9 @@ else
     # weighted_average=0
     sql="(select avg(loss) from packet_loss)"
 fi
-# log_it "weighted_average [$weighted_average]"
-# unset weighted_average
-
 
 current_loss="$(sqlite3 "$db" "select round($sql,1)")"
 # log_it "raw loss [$current_loss]"
-
-
-lvl_disp="$(get_tmux_option "@packet-loss_level_disp" "$default_lvl_display")"
-# log_it "lvl_disp [$lvl_disp]"
-
 
 if [ "$(echo "$current_loss < $lvl_disp" | bc)" -eq 1 ]; then
     # log_it "$current_loss is below threshold $lvl_disp"
@@ -107,38 +98,25 @@ fi
 #  To minimize CPU hogging, only fetch options when needed
 #
 if [ -n "$current_loss" ]; then
-    lvl_crit="$(get_tmux_option "@packet-loss_level_crit" "$default_lvl_crit")"
-    # log_it "lvl_crit [$lvl_crit]"
-
-    lvl_alert="$(get_tmux_option "@packet-loss_level_alert" "$default_lvl_alert")"
-    # log_it "lvl_alert [$lvl_alert]"
     #
     #  If loss over trigger levels, display in appropriate color
     #
     if awk -v val="$current_loss" -v trig_lvl="$lvl_crit" 'BEGIN{exit !(val >= trig_lvl)}'; then
-        color_crit="$(get_tmux_option "@packet-loss_color_crit" "$default_color_crit")"
-        # log_it "color_crit [$color_crit]"
-        color_bg="$(get_tmux_option "@packet-loss_color_bg" "$default_color_bg")"
-        # log_it "color_bg [$color_bg]"
-        current_loss="#[fg=$color_crit,bg=$color_bg]$current_loss%#[default]"
+        current_loss="#[fg=$color_crit,bg=$color_bg]$current_loss#[default]"
     elif awk -v val="$current_loss" -v trig_lvl="$lvl_alert" 'BEGIN{exit !(val >= trig_lvl)}'; then
-        color_alert="$(get_tmux_option "@packet-loss_color_alert" "$default_color_alert")"
-        # log_it "color_alert [$color_alert]"
-        color_bg="$(get_tmux_option "@packet-loss_color_bg" "$default_color_bg")"
-        # log_it "color_bg [$color_bg]"
-        current_loss="#[fg=$color_alert,bg=$color_bg]$current_loss%#[default]"
-    else
-        current_loss="$current_loss%"
+        current_loss="#[fg=$color_alert,bg=$color_bg]$current_loss#[default]"
     fi
 
-    loss_prefix="$(get_tmux_option "@packet-loss_prefix" "$default_prefix")"
-    # log_it "loss_prefix [$loss_prefix]"
-
-    loss_suffix="$(get_tmux_option "@packet-loss_suffix" "$default_suffix")"
-    # log_it "loss_suffix [$loss_suffix]"
+    if bool_param "$hist_avg_display"; then
+        avg_loss="$(sqlite3 "$db" "select round(avg(loss),1) from statistics")"
+        if [ ! "$avg_loss" = "0" ]; then
+            current_loss="$current_loss~$avg_loss"
+        fi
+    fi
 
     current_loss="$loss_prefix$current_loss$loss_suffix"
     log_it "reported loss [$current_loss]"
+
 fi
 
 echo "$current_loss"
--- scripts/packet_loss_monitor.sh
+++ scripts/packet_loss_monitor.sh
@@ -10,15 +10,15 @@
 #   Version: 1.1.4 2022-04-06
 #
 
-
 # shellcheck disable=SC1007
 CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 # shellcheck disable=SC1091
 . "$CURRENT_DIR/utils.sh"
 
+get_settings
+
 db="$(dirname -- "$CURRENT_DIR")/data/$sqlite_db"
 pidfile="$(dirname -- "$CURRENT_DIR")/data/$monitor_pidfile"
-
 
 #
 #  Since loss is treated as a float, indicate errors with results over 100
@@ -35,7 +35,6 @@ error_no_ping_output="101"
 # `ping_cmd | grep loss`  gave empty result, unlikely to self correct
 error_unable_to_detect_loss="201"
 
-
 #
 #  Ensure only one instance is running.
 #
@@ -43,20 +42,17 @@ if [ -e "$pidfile" ]; then
     error_msg "$monitor_process_scr seems to already be running, aborting!" 1
 fi
 
-
 #
 #  Save pid for easier kill from packet-loss.tmux
 #
 log_it "Saving new pid [$$] into pidfile"
-echo "$$" > "$pidfile"
-
+echo "$$" >"$pidfile"
 
 #
 #  Getting parameters from DB
 #
 ping_count="$(sqlite3 "$db" "SELECT ping_count FROM params")"
 host="$(sqlite3 "$db" "SELECT host FROM params")"
-
 
 #
 #  Figuring out the nature of the available ping cmd
@@ -66,7 +62,7 @@ host="$(sqlite3 "$db" "SELECT host FROM params")"
 #  Detecting what ping command is present, in order to figure out the
 #  right timeout param
 #
-timeout_help="$(ping -h 2> /dev/stdout| grep timeout)"
+timeout_help="$(ping -h 2>/dev/stdout | grep timeout)"
 
 if [ "${timeout_help#*-t}" != "$timeout_help" ]; then
     timeout_parameter="t"
@@ -93,8 +89,8 @@ log_it "$monitor_process_scr will use ping cmd [$ping_cmd]"
 #
 #  Main
 #
-while : ; do
-    output="$($ping_cmd  | grep loss)"
+while :; do
+    output="$($ping_cmd | grep loss)"
     if [ -n "$output" ]; then
         #
         #  We cant rely on the absolute position of the %loss, since sometimes it is prepended with stuff like:
@@ -106,7 +102,7 @@ while : ; do
         #  4 only keep line up to not including ~ (packet loss)
         #  5 display last remaining word - packet loss as a float with no % sign!
         #
-        percent_loss="$(echo "$output" | sed 's/packet loss/~/ ; s/%//' | cut -d~ -f 1 | awk 'NF>1{print $NF}' )"
+        percent_loss="$(echo "$output" | sed 's/packet loss/~/ ; s/%//' | cut -d~ -f 1 | awk 'NF>1{print $NF}')"
         if [ -z "$percent_loss" ]; then
             error_msg "Failed to parse ping output, unlikely to self correct!"
             percent_loss="$error_unable_to_detect_loss"
@@ -125,5 +121,13 @@ while : ; do
         sleep "$ping_count"
     fi
     sqlite3 "$db" "INSERT INTO packet_loss (loss) values ($percent_loss)"
+
+    #  Add one line in statistics each minute
+    sql="SELECT COUNT(*) FROM statistics WHERE t_stamp >= datetime(strftime('%Y-%m-%d %H:%M'))"
+    items_this_minute="$(sqlite3 "$db" "$sql")"
+    if [ "$items_this_minute" -eq 0 ]; then
+        sqlite3 "$db" 'INSERT INTO statistics (loss) SELECT round(avg(loss),1) FROM log_1_min'
+    fi
+
     log_it "stored in DB: $percent_loss"
 done

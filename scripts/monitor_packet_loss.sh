e8ef1e0017ff18ecfa6bd1a32ed89eafd040b79b
jaclu <jacob.lundqvist@gmail.com>
2024-04-17 14:15:40 +0200

    activated logging of losses

--- scripts/monitor_packet_loss.sh
+++ scripts/monitor_packet_loss.sh
@@ -145,5 +145,5 @@ while :; do
     fi
 
     #  A bit exessive in normal conditions
-    #[[ "$percent_loss" != "0.0" ]] && log_it "stored in DB: $percent_loss"
+    [[ "$percent_loss" != "0.0" ]] && log_it "stored in DB: $percent_loss"
 done

0bd87f7f4833f2eb3c1c2781f7cd756c04c306f5
jaclu <jacob.lundqvist@gmail.com>
2024-04-21 15:50:34 +0200

    handles no session

--- scripts/show_settings.sh
+++ scripts/show_settings.sh
@@ -29,21 +29,34 @@ show_item() {
     echo "$msg"
 }
 
-echo "=====   Config for  session: $(get_tmux_socket)   ====="
+session="$(get_tmux_socket)"
+
+echo "=====   Config for  session: $session   ====="
+
+[[ "$session" = "standalone" ]] && {
+    echo
+    echo "*** This is not inside any tmux session - only defaults will be displayed!"
+    echo
+
+    use_param_cache=false
+    get_settings
+}
+
 ping_count="$(show_item cfg_ping_count "$cfg_ping_count" "$default_ping_count")"
 echo "$ping_count"
 
-status_interval="$($TMUX_BIN display -p "#{status-interval}" 2>/dev/null)"
-
-if [[ -n "$status_interval" ]]; then
-    msg_interval="status bar update frequency = [$status_interval]"
-    req_interval="$(echo "$cfg_ping_count - 1" | bc)"
-    if [[ "$req_interval" != "$status_interval" ]]; then
-        msg_interval="$msg_interval  - tmux status-interval is recomended to be arround: $req_interval"
+[[ "$session" != "standalone" ]] && {
+    status_interval="$($TMUX_BIN display -p "#{status-interval}" 2>/dev/null)"
+    if [[ -n "$status_interval" ]]; then
+        msg_interval="status bar update frequency = [$status_interval]"
+        req_interval="$(echo "$cfg_ping_count - 1" | bc)"
+        if [[ "$req_interval" != "$status_interval" ]]; then
+            msg_interval="$msg_interval  - tmux status-interval is recomended to be arround: $req_interval"
+        fi
+        echo "$msg_interval"
+        echo
     fi
-    echo "$msg_interval"
-    echo
-fi
+}
 
 show_item cfg_ping_host "$cfg_ping_host" "$default_ping_host"
 show_item cfg_history_size "$cfg_history_size" "$default_history_size"
@@ -68,23 +81,3 @@ show_item cfg_prefix "$cfg_prefix" "$default_prefix"
 show_item cfg_suffix "$cfg_suffix" "$default_suffix"
 
 show_item cfg_hook_idx "$cfg_hook_idx" "$default_hook_idx"
-
-# The rest depends on a tmux session
-[[ -z "$TMUX" ]] && exit 0
-
-echo
-echo "===   temp variables stored in tmux by scripts/display_losses.sh   ==="
-
-# used to indicate trends
-opt_last_value="@packet-loss_tmp_last_value"
-
-# for caching
-opt_last_check="@packet-loss_tmp_last_check"
-opt_last_result="@packet-loss_tmp_last_result"
-
-echo "last_check  [$(get_tmux_option "$opt_last_check" \
-    "$opt_last_check unset")]"
-echo "last_value  [$(get_tmux_option "$opt_last_value" \
-    "$opt_last_value unset")]"
-echo "last_result [$(get_tmux_option "$opt_last_result" \
-    "$opt_last_result unset")]"

e73d74612c47dc37f899a7c69eb6b902619d17af
jaclu <jacob.lundqvist@gmail.com>
2024-04-21 16:54:26 +0200

    disabled logging

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -262,7 +262,7 @@ display_time_elapsed() {
 #  Shorthand, to avoid manually typing package name on multiple
 #  locations, easily getting out of sync.
 #
-plugin_name="tmux-devel-packet-loss"
+plugin_name="tmux-packet-loss"
 
 #
 #  log_it is used to display status to $log_file if it is defined.
@@ -271,7 +271,7 @@ plugin_name="tmux-devel-packet-loss"
 #  If $log_file is unset no output will happen.
 #  So unless you want logging, comment the next line out.
 #
-log_file="/tmp/${plugin_name}.log"
+# log_file="/tmp/${plugin_name}.log"
 
 #  used in logfile to indicate what tool that generated output
 [[ -z "$log_prefix" ]] && log_prefix="???"

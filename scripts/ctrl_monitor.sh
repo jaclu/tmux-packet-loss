245c5cbd947179a6474de9687b2860115ccb3073
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2024-04-23 08:07:03 +0200

    fixed path

--- scripts/ctrl_monitor.sh
+++ scripts/ctrl_monitor.sh
@@ -50,7 +50,7 @@ hook_handler() {
 clear_losses_in_t_loss() {
     [[ -n "$($scr_display_losses)" ]] && {
         log_it "Clearing losses - to ensure plugin isnt stuck alerting"
-        sqlite3 data/packet_loss.sqlite "DELETE FROM t_loss WHERE loss != 0" || {
+        sqlite3 "$sqlite_db" "DELETE FROM t_loss WHERE loss != 0" || {
             error_msg "sqlite3 reported error:[$?] in clear_losses_in_t_loss()"
         }
     }

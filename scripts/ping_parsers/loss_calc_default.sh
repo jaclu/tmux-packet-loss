083f6c5376de363182fb235a4be8393c19f43bec
jaclu <jacob.lundqvist@gmail.com>
2024-04-26 10:41:33 +0200

    moved the loss checkers

---
+++ scripts/ping_parsers/loss_calc_default.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+#   Copyright (c) 2024: Jacob.Lundqvist@gmail.com
+#   License: MIT
+#
+#   Part of https://github.com/jaclu/tmux-packet-loss
+#
+#    Default ping parser
+#
+#  We cant rely on the absolute position of the %loss,
+#  since sometimes it is prepended with stuff like: "+1 duplicates,"
+#
+#  To handle this we search for "packet loss" and use the word
+#  just before it.
+#
+#  1 Only bother with the line containing the word loss
+#  2 replace "packet loss" with ~, since cut needs a single char
+#    delimiter
+#  3 remove any % chars, we want loss as a float
+#  4 only keep line up to not including ~ (packet loss)
+#  5 display last remaining word - packet loss as a float with
+#    no % sign!
+#
+
+if [[ -n "$1" ]]; then
+    ping_output="$1"
+else
+    # Read input from stdin
+    ping_output=$(cat)
+fi
+
+echo "$ping_output" | grep "packet loss" |
+    sed 's/packet loss/~/ ; s/%//' | cut -d~ -f 1 | awk 'NF>1{print $NF}'
--- scripts/ping_parsers/loss_calc_ish_deb10.sh
+++ scripts/ping_parsers/loss_calc_ish_deb10.sh
@@ -9,20 +9,40 @@
 #  often negative loss numbers and sometimes gives replies
 #  for other hosts - (def gw?)
 #  and often replies tagged with DUP
+#  since we have to filter out any 8.8.8.8 results
+#  this ping parser can not use ping host 8.8.8.8!
 #
 #  Here we instead count the number of correct replies and do the
 #  math ourself
 #
 
-D_TPL_BASE_PATH=$(dirname "$(dirname -- "$(realpath "$0")")")
+#
+#  sourcing utils.sh is needed, scine This one needs to use some
+#  config variables:
+#    cfg_ping_host $recieved_packets  $cfg_ping_count
+#
+D_TPL_BASE_PATH=$(dirname "$(dirname "$(dirname -- "$(realpath "$0")")")")
 log_prefix="png"
 
 #  shellcheck source=scripts/utils.sh
 . "$D_TPL_BASE_PATH/scripts/utils.sh"
 
+[[ "$cfg_ping_host" = "8.8.8.8" ]] && {
+    this_app="$(basename "$0")"
+    msg="$this_app cant use 8.8.8.8 as ping host"
+    if [[ -n "$log_file" ]]; then
+        log_it "$msg"
+    else
+        error_msg "$msg" 1
+    fi
+}
 
-# Read input from stdin
-ping_output=$(cat)
+if [[ -n "$1" ]]; then
+    ping_output="$1"
+else
+    # Read input from stdin
+    ping_output=$(cat)
+fi
 
 #
 #  with most pings ' icmp_seq=' can be used to identify a reply
@@ -34,24 +54,8 @@ recieved_packets="$(echo "$ping_output" | grep -v DUP | grep "seq=" |
     grep "$cfg_ping_host" | wc -l)"
 
 #
-#  Sometimes this gets extra replies fom 8.8.8.8
-#  If 8.8.4.4 is pinged filtering on $cfg_ping_host gets rid of them,
-#  if 8.8.8.8 is the pinghost this will signal results over 100
-#
-#  Did a quick fix, but will leave it commented out for now to gather
-#  some more stats on how often this happens.
-#
-# [[ "$recieved_packets" -gt "$cfg_ping_count" ]] && {
-#     recieved_packets="$cfg_ping_count"
-# }
-
-
-#
 #  bc rounds 33.3333 to 33.4 to work arround this, bc uses two digits
 #  printf rounds it down to one
 #
-percent_loss="$(echo "scale=2;
-    100 - 100 * $recieved_packets / $cfg_ping_count" | bc | 
-    awk '{printf "%.1f", $0}')"
-
-echo "$percent_loss"
+echo "scale=2; 100 - 100 * $recieved_packets / $cfg_ping_count" | bc |
+    awk '{printf "%.1f", $0}'

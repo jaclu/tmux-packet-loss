bba095e58a95b05db3b97f95f3659e4b13f35a3d
jaclu <jacob.lundqvist@gmail.com>
2024-04-26 10:40:18 +0200

    Sanity checks handling the weirdness of busybox ping

--- scripts/monitor_packet_loss.sh
+++ scripts/monitor_packet_loss.sh
@@ -7,15 +7,50 @@
 #
 #   This runs forever.
 #
+float_digits() {
+    local value="$1"
+    local digits="$2"
+
+    [[ -z "$digits" ]] && {
+        error_msg "rouns_float($value,) - missing digits param" 1 false
+    }
+    printf "%.${digits}f" "$value"
+}
+
+float_2_int() {
+    float_digits "$1" 0
+}
+
+float_drop_digits() {
+    #
+    # float to int by dropping all digits
+    #
+    echo "$1" | sed 's/\./ /' | cut -d' ' -f 1
+}
+
+is_int() {
+    case $1 in
+    '' | *[!0-9]*) return 1 ;; # Contains non-numeric characters
+    *) return 0 ;;             # Contains only digits
+    esac
+}
 
 define_ping_cmd() {
     #
     #  Figuring out the nature of the available ping cmd
     #  Variables provided:
     #    ping_cmd - options adjusted for the local environment
+    #    is_busybox_ping - true if it was busybox
     #
     local timeout_help
     local timeout_parameter
+
+    if realpath "$(command -v ping)" | grep -qi busybox; then
+        is_busybox_ping=true
+    else
+        is_busybox_ping=false
+    fi
+    $is_busybox_ping && log_it "system useses BusyBox ping"
 
     #
     #  Selecting the right timeout option
@@ -44,7 +79,7 @@ define_ping_cmd() {
     log_it "ping cmd used: [$ping_cmd]"
 }
 
-calculate_loss_default() {
+not_calculate_loss_default() {
     #  We cant rely on the absolute position of the %loss,
     #  since sometimes it is prepended with stuff like:
     #  "+1 duplicates,"
@@ -59,11 +94,10 @@ calculate_loss_default() {
     #    no % sign!
     #
     percent_loss="$(echo "$output" | grep "packet loss" |
-        sed 's/packet loss/~/ ; s/%//' | cut -d~ -f 1 | awk 'NF>1{print $NF}' |
-        awk '{printf "%.1f", $0}')" # last line ensures it's correctly rounded
+        sed 's/packet loss/~/ ; s/%//' | cut -d~ -f 1 | awk 'NF>1{print $NF}')"
 }
 
-calculate_loss_ish_deb10() {
+not_calculate_loss_ish_deb10() {
     #
     #  with most pings ' icmp_seq=' can be used to identify a reply
     #  Obviously busybox uses ' seq=' ...
@@ -90,8 +124,7 @@ calculate_loss_ish_deb10() {
     #  printf rounds it down to one
     #
     percent_loss="$(echo "scale=2;
-        100 - 100 * $recieved_packets / $cfg_ping_count" | bc |
-        awk '{printf "%.1f", $0}')"
+    100 - 100 * $recieved_packets / $cfg_ping_count" | bc)"
 }
 
 ping_parse_error() {
@@ -102,6 +135,56 @@ ping_parse_error() {
 
     log_it "*** ping parsing error - $err_msg [$percent_loss]"
     percent_loss="$err_code"
+}
+
+compare_loss_parsers() {
+    #
+    #  When alternate loss calculations are used
+    #  this compares with what the default would give
+    #  and logs items that might need further inspection
+    #  It is not an error in it-self if they differ, but it is
+    #  quick way to gather sample data for testing.
+    #
+    local alt_percentage_loss
+    local msg
+    local iso_datetime
+    local f_ping_issue
+    #
+    #  an alternete check detected a loss
+    #  compare result with what default check gives
+    #  and log the output if they differ
+    #
+    log_it "Double checking loss calculation"
+    alt_percentage_loss="$percent_loss"
+    ! is_busybox_ping && [[ "$loss_check" = "$scr_loss_ish_deb10" ]] && {
+        #  in the summary ping is sometimes given with 4 digits...
+        percent_loss=$(float_digits "$(echo "$output" | $scr_loss_default)" 1)
+    }
+    is_int "$percent_loss" && {
+        #
+        #  if default used no digits, round the alt to int in order to
+        #  avoid irrelevant differences
+        #
+        if $is_busybox_ping; then
+            #
+            #  Allways round it down by dropping digits,
+            #  busybox ping is one of a kind...
+            #
+            alt_percentage_loss=$(float_drop_digits "$alt_percentage_loss")
+        else
+            alt_percentage_loss=$(float_2_int "$alt_percentage_loss")
+        fi
+    }
+    [[ "$percent_loss" != "$alt_percentage_loss" ]] && {
+        msg="This alternate[$alt_percentage_loss] and "
+        msg+="default[$percent_loss] loss check differ"
+        log_it "$msg"
+        mkdir -p "$d_ping_history"
+        iso_datetime=$(date +'%Y-%m-%d_%H:%M:%S')
+        f_ping_issue="$d_ping_history/$iso_datetime"
+        log_it "Saving ping issue at: $f_ping_issue"
+        echo "$output" >"$f_ping_issue"
+    }
 }
 
 #===============================================================
@@ -116,16 +199,11 @@ log_prefix="mon"
 #  shellcheck source=scripts/utils.sh
 . "$D_TPL_BASE_PATH/scripts/utils.sh"
 
-# If true, output of pings with issues will be saved
-store_ping_issues=false
-
-d_ping_history="$d_data"/ping_issues
-
 #
 #  Include pidfile handling
 #
 # shellcheck source=scripts/pidfile_handler.sh
-. "$D_TPL_BASE_PATH"/scripts/pidfile_handler.sh
+. "$scr_pidfile_handler"
 
 pidfile_acquire "$pidfile_monitor" || {
     error_msg "$pidfile_monitor - is owned by process [$pidfile_proc]"
@@ -133,8 +211,13 @@ pidfile_acquire "$pidfile_monitor" || {
 
 pidfile_is_live "$pidfile_tmux" || error_msg "tmux pidfile not found!"
 
+# If true, output of pings with issues will be saved
+store_ping_issues=false
+
+d_ping_history="$d_data"/ping_issues
+
 #
-#  Since loss is <=100, indicate errors with results over 100
+#  Since loss is always <=100, indicate errors with results over 100
 #  Not crucial to remember exactly what it means,
 #  enough to know is >100 means monitor error
 #
@@ -156,19 +239,22 @@ error_unable_to_detect_loss=201
 
 define_ping_cmd # we need the ping_cmd in kill_any_strays
 
+scr_loss_default="$D_TPL_BASE_PATH"/scripts/ping_parsers/loss_calc_default.sh
+scr_loss_ish_deb10="$D_TPL_BASE_PATH"/scripts/ping_parsers/loss_calc_ish_deb10.sh
+
 #
 #  Check if special handling of output is needed
 #
-if [[ -d /proc/ish ]] && grep -q '10.' /etc/debian_version; then
+if [[ -d /proc/ish ]] && grep -q '10.' /etc/debian_version 2>/dev/null; then
     store_ping_issues=true
-    loss_check=calculate_loss_ish_deb10
+    loss_check="$scr_loss_ish_deb10"
 else
-    loss_check=calculate_loss_default
+    loss_check="$scr_loss_default"
 fi
-log_it "Checking losses using: $loss_check"
-
+log_it "Checking losses using: $(basename "$loss_check")"
 $store_ping_issues && log_it "Will save ping issues in $d_ping_history"
 
+#  Ensure DB and all triggers are vallid
 "$D_TPL_BASE_PATH"/scripts/prepare_db.sh
 
 #
@@ -195,8 +281,10 @@ while true; do
     #
     output="$($ping_cmd 2>/dev/null)"
     if [[ -n "$output" ]]; then
-        $loss_check
-
+        percent_loss="$(echo "$output" | $loss_check)" || {
+            log_it "$(basename "$loss_check") returned error" 1
+            exit 1
+        }
         if [[ -z "$percent_loss" ]]; then
             msg="Failed to parse ping output, unlikely to self correct!"
             ping_parse_error "$error_unable_to_detect_loss" "$msg"
@@ -221,6 +309,13 @@ while true; do
         sleep "$cfg_ping_count"
     fi
 
+    #
+    #  Many environments lists average losses with one decimal, but not all,
+    #  BusyBox is one example. In the end, all that is needed is an agreement
+    #  on what describes no losses.
+    #
+    [[ "$percent_loss" = 0.0 ]] && percent_loss=0
+
     sqlite_err_handling "INSERT INTO t_loss (loss) VALUES ($percent_loss)" || {
         err_code=$?
         if [[ "$err_code" = 5 ]]; then
@@ -231,30 +326,12 @@ while true; do
         fi
         continue
     }
-    #  A bit exessive in normal conditions
-    [[ "$percent_loss" != "0.0" ]] && log_it "stored in DB: $percent_loss"
+    [[ "$percent_loss" != 0 ]] && {
+        log_it "stored in DB: $percent_loss"
 
-    $store_ping_issues && ! $parse_error && [[ "$percent_loss" != "0.0" ]] && {
-        [[ "$loss_check" != "calculate_loss_default" ]] && {
-            #
-            #  an alternete check detected a loss
-            #  compare result with what default check gives
-            #  and log the output if they differ
-            #
-            log_it "Double checking loss calculation"
-            alt_percentage_loss="$percent_loss"
-            calculate_loss_default
-            [[ "$percent_loss" != "$alt_percentage_loss" ]] && {
-                msg="This alternate[$alt_percentage_loss] and "
-                msg+="default[$percent_loss] loss check differ"
-                log_it "$msg"
-                mkdir -p "$d_ping_history"
-                iso_datetime=$(date +'%Y-%m-%d_%H-%M-%S')
-                f_ping_issue="$d_ping_history/$iso_datetime"
-                log_it "Saving ping issue at: $f_ping_issue"
-                echo "$output" >"$f_ping_issue"
-            }
-        }
+        $store_ping_issues && ! $parse_error &&
+            [[ "$loss_check" != "$scr_loss_default" ]] &&
+            compare_loss_parsers
     }
 
     #
@@ -285,10 +362,18 @@ while true; do
 
         #
         #  By calling this in the background, this process can kill itself
-        #  helping iSH
+        #  reducing risk of iSH craching
         #
         $scr_ctrl_monitor shutdown &
         break
+    }
+    $parse_error && {
+        #
+        #  in order not to constantly loop and potentially
+        #  flooding the log_file
+        #
+        log_it "Sleeping due to parse error"
+        sleep 10
     }
 done
 

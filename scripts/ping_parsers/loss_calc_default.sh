aea343eb4ec4b5e03f19ba39931237b2e9dc2113
jaclu <jacob.lundqvist@gmail.com>
2024-04-27 20:35:52 +0200

    rewrote loss_calc scripts

--- scripts/ping_parsers/loss_calc_default.sh
+++ scripts/ping_parsers/loss_calc_default.sh
@@ -5,7 +5,22 @@
 #
 #   Part of https://github.com/jaclu/tmux-packet-loss
 #
-#    Default ping parser
+#  Default ping parser
+#
+#  Either give file to read as a param, or pipe ping output into this
+#
+
+D_TPL_BASE_PATH=$(dirname "$(dirname "$(dirname -- "$(realpath "$0")")")")
+log_prefix="png"
+source "$D_TPL_BASE_PATH"/scripts/utils.sh
+
+if [[ -n "$1" ]]; then
+    ping_output="$(cat "$1")"
+else
+    # Read input from stdin
+    ping_output="$(cat)"
+fi
+
 #
 #  We cant rely on the absolute position of the %loss,
 #  since sometimes it is prepended with stuff like: "+1 duplicates,"
@@ -21,21 +36,45 @@
 #  5 display last remaining word - packet loss as a float with
 #    no % sign!
 #
+avg_loss="$(echo "$ping_output" |
+    grep "packet loss" | # Only process the summary line
 
-if [[ -n "$1" ]]; then
-    ping_output="$1"
-else
-    # Read input from stdin
-    ping_output=$(cat)
-fi
+    # get rid of % and make ~ indicate end of interesting part
+    sed 's/%// ; s/packet loss/~/' |
+    cut -d~ -f 1 | # only keep up to before ~
+    awk 'NF>1{print $NF}')"
 
 #
-#  some pings occationally end up giving 4 decimals on the
-#  average loss, the last step rounds it down to just one
+#  Normalize number of decimals
 #
-echo "$ping_output" |
-    grep "packet loss" |
-    sed 's/packet loss/~/ ; s/%//' |
-    cut -d~ -f 1 |
-    awk 'NF>1{print $NF}' |
-    awk '{printf "%.1f", $0}'
+case $(echo "$avg_loss" | awk -F'.' '{ print length($2) }') in
+1) ;;
+0) # fake a decimal
+    # log_it "faked one decimal"
+    avg_loss="${avg_loss}.0"
+    ;;
+*) # only use one digit
+    rounded_loss="$(echo "$avg_loss" | awk '{printf "%.1f", $0}')"
+    log_it "odd avg loss, got [$avg_loss] expected [$rounded_loss]"
+    avg_loss="$rounded_loss"
+    ;;
+esac
+
+# #
+# #  BusyBox occationally end up giving 4 decimals on the average loss,
+# #  this checks for such, and rounds it down to just the normal one
+# #
+# echo "$avg_loss" | awk -F'.' '{ if (length($2) > 1) exit 0; else exit 1 }' && {
+#     # only use one digit
+#     rounded_loss="$(echo "$avg_loss" | awk '{printf "%.1f", $0}')"
+#     log_it "odd loss, got [$avg_loss] expected [$rounded_loss]"
+#     avg_loss="$rounded_loss"
+
+#     is_busybox_ping && {
+#         # shorten it down further to dropbox notation
+#         avg_loss="$(float_drop_digits "$avg_loss")"
+#         log_it "rounded it down to busybox no digits: $avg_loss"
+#     }
+# }
+
+echo "$avg_loss"
--- scripts/ping_parsers/loss_calc_ish_deb10.sh
+++ scripts/ping_parsers/loss_calc_ish_deb10.sh
@@ -15,56 +15,70 @@
 #  Here we instead count the number of correct replies and do the
 #  math ourself
 #
+#  Either give file to read as a param, or pipe ping output into this
+#  If read is first param ping settings are extracted from ping output
+#
 
-#
-#  sourcing utils.sh is needed, scine This one needs to use some
-#  config variables:
-#    cfg_ping_host $recieved_packets  $cfg_ping_count
-#
 D_TPL_BASE_PATH=$(dirname "$(dirname "$(dirname -- "$(realpath "$0")")")")
-log_prefix="png"
+log_prefix="png-i"
+source "$D_TPL_BASE_PATH"/scripts/utils.sh
 
-#  shellcheck source=scripts/utils.sh
-. "$D_TPL_BASE_PATH/scripts/utils.sh"
+if [[ "$1" = "read" ]]; then
+    #  When testing, read settings from output to match sample
+    shift
+    extract_settings=true
+else
+    extract_settings=false
+fi
+
+if [[ -n "$1" ]]; then
+    ping_output="$(cat "$1")"
+else
+    # Read input from stdin
+    ping_output="$(cat)"
+fi
+
+$extract_settings && {
+    cfg_ping_host="$(echo "$ping_output" | grep "^PING" | cut -d' ' -f 2)"
+    cfg_ping_count="$(echo "$ping_output" | grep "packets transmitted" |
+        cut -d' ' -f 1)"
+    echo "Extracted"
+    echo "cfg_ping_host:  $cfg_ping_host"
+    echo "cfg_ping_count: $cfg_ping_count"
+    echo
+}
 
 [[ "$cfg_ping_host" = "8.8.8.8" ]] && {
     this_app="$(basename "$0")"
     msg="$this_app cant use 8.8.8.8 as ping host"
-    if [[ -n "$log_file" ]]; then
-        log_it "$msg"
-    else
-        error_msg "$msg" 1
-    fi
+    $extract_settings && echo "$msg"
+    error_msg "$msg" 0 false
+    echo 201
+    exit 0
 }
-
-if [[ -n "$1" ]]; then
-    ping_output="$1"
-else
-    # Read input from stdin
-    ping_output=$(cat)
-fi
 
 #
 #  with most pings ' icmp_seq=' can be used to identify a reply
 #  Obviously busybox uses ' seq=' ...
 #
-
 #  shellcheck disable=SC2126
-recieved_packets="$(echo "$ping_output" | grep -v DUP | grep "seq=" |
+result="$(echo "$ping_output" | grep -v DUP | grep "seq=" |
     grep "$cfg_ping_host" | wc -l)"
+#  Trims white space
+recieved_packets="${result#"${result%%[![:space:]]*}"}"
 
-#
-#  Sometimes this gets extra replies
-#  Did a quick fix, but will leave it commented out for now to gather
-#  some more stats on how often this happens.
-#
-# [[ "$recieved_packets" -gt "$cfg_ping_count" ]] && {
-#     recieved_packets="$cfg_ping_count"
-# }
-
-#
-#  bc rounds 33.3333 to 33.4 to work arround this, bc uses three digits
-#  in order to give the printf better source data as it rounds it down to one
-#
-echo "scale=3; 100 - 100 * $recieved_packets / $cfg_ping_count" | bc |
-    awk '{printf "%.1f", $0}'
+if [[ "$recieved_packets" -gt "$cfg_ping_count" ]]; then
+    #
+    #  Sometimes BusyBox ping gets extra replies, this filters them out
+    #
+    error_msg "got $recieved_packets pkts, expected $cfg_ping_count" 0 false
+    echo 0.0
+else
+    #
+    #  bc rounds 33.3333 to 33.4 to work arround this, bc uses three digits
+    #  in order to give the printf better source data as it rounds it down
+    #  to one
+    #
+    echo "scale=3; 100 - 100 * $recieved_packets / $cfg_ping_count" |
+        bc | awk '{printf "%.1f\n", $0}'
+fi

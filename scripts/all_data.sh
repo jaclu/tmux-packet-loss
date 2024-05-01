a0c44762bf62a5a486114491ba1d89bb8c154097
jaclu <jacob.lundqvist@gmail.com>
2024-05-01 11:39:46 +0200

    new script to show/clear DB

---
+++ scripts/all_data.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+#   Copyright (c) 2024: Jacob.Lundqvist@gmail.com
+#   License: MIT
+#
+#   Part of https://github.com/jaclu/tmux-packet-loss
+#
+#  Show / Clear content of all tables
+#
+app_name=$(basename "$0")
+
+D_TPL_BASE_PATH=$(dirname "$(dirname -- "$(realpath "$0")")")
+log_prefix="a_d"
+
+#  shellcheck source=scripts/utils.sh
+. "$D_TPL_BASE_PATH"/scripts/utils.sh
+
+action="$1"
+
+case "$action" in
+show)
+    cmd="SELECT *"
+    ;;
+clear)
+    cmd="DELETE"
+    log_it "DB will be cleared"
+    ;;
+*)
+    echo "usage: $app_name show/clear"
+    exit 1
+    ;;
+esac
+
+tables=$(sqlite3 "$sqlite_db" ".tables")
+for table in $tables; do
+    echo "-----  Table: $table"
+    sqlite3 "$sqlite_db" "$cmd FROM $table;"
+    echo "average: $(
+        sqlite3 "$sqlite_db" "SELECT round(avg(loss),1) FROM $table;"
+    )"
+    echo
+done
